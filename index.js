e7811a966f48ec2e8a650f190d907b1f83ae6850
Rejin Cusi <rejincusi@gmail.com>
2019-08-09 10:42:43 +0200

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const Sse = require('json-sse')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = process.env.PORT || 5000
 
+const data = 'hello world'
+const sse = new Sse(data)
+
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
+
+app.get('/stream', sse.init)
+
+app.post('/message', (req, res) => {
+  const { message } = req.body
+    sse.send(message)
+
+    res.send(message)
+  }
+)
 
 app.listen(port, () => console.log(`Listening on :${port}`))
